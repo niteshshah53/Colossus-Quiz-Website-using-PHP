b74c5ab7fd3ae8ff96b4acda9246459c54be92dd
Nitesh Kumar Shah <2053nitesh@gmail.com>
2023-03-25 21:16:56 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,81 @@
+<!Doctype html>
+<html lang="en">
+<head>
+<title>My web site</title>
+<meta charset="utf-8"/>
+<link rel="stylesheet" type="text/css" href="style1.css">
+<style type="text/css">
+.mainheader img{
+	width:100%;
+	height: 60px;
+}
+
+</style>
+<meta name="viewpor" content="width=device-width,initial-scale=1.0" >
+</head>
+<body class="body">
+	<header class="mainheader">
+		<img src="images/1.jpg" >
+		<nav>
+			<ul>
+				<li ><a href="index.php" >Home</a></li>
+				<li class="active"><a href="#">About</a></li>
+				<li><a href="add.php">Add question</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="signin.php">Sign IN</a></li>
+
+
+			</ul>
+
+		</nav>
+	</header>
+	<div class="maincontent">
+		<div class="content">
+			<article class="toparticle">
+				<header>
+					<h2><a href="#" title="firstpost"></a></h2>
+
+				</header>
+				<footer class="post-info">
+					<p>This post is written by Nitesh 2018/1/20</p>
+				</footer>
+				<content>
+
+              <img src="images/dd.gif">
+
+
+				</content>
+
+			</article>
+				
+
+		</div>
+
+	</div>
+	<aside class="top-sidebar">
+		<article>
+		<h2>Top sidebar</h2>
+		<p>Therefore the Qur'an is named The Criterion, 
+					the criterion between right and wrong, good and evil.</p>
+		</article>
+	</aside>
+	<aside class="middle-sidebar">
+		<article>
+		<h2>Middle sidebar</h2>
+		<p>Therefore the Qur'an is named The Criterion, 
+					the criterion between right and wrong, good and evil.</p>
+		</article>
+	</aside>
+	<aside class="bottom-sidebar">
+		<article>
+		<h2>bottom sidebar</h2>
+		<p>Therefore the Qur'an is named The Criterion, 
+					the criterion between right and wrong, good and evil.</p>
+		</article>
+	</aside>
+	<footer class="mainFooter">
+		<p>copyright@<a href="#" title="myweb.com">myweb.com</a></p>
+	</footer>
+
+</body>
+</html>
