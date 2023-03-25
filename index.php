3be37da20653862f449dadc8f551b9f76cba88a8
Nitesh Kumar Shah <2053nitesh@gmail.com>
2023-03-25 21:19:46 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,116 @@
+<!Doctype html>
+<html lang="en">
+<head>
+<title>My web site</title>
+<meta charset="utf-8"/>
+<link rel="stylesheet" type="text/css" href="style1.css">
+<style type="text/css">
+.mainheader img{
+	width:100%;
+	height: 90px;
+}
+
+</style>
+<meta name="viewpor" content="width=device-width,initial-scale=1.0" >
+</head>
+<body class="body">
+	<header class="mainheader">
+		<img src="images/brcen.jpg" >
+		<nav>
+			<ul>
+				
+				   <li class="active"><a href="index.php"  >Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+				<li><a href="signin.php">Sign In</a></li>
+				<li><a href="#">Add question</a></li>
+			
+
+			</ul>
+
+		</nav>
+	</header>
+	<div class="maincontent">
+		<div class="content">
+			<article class="toparticle">
+				<header>
+					<h2><a href="#" title="firstpost">HOW TO STUDY BETTER</a></h2>
+
+				</header>
+				<footer class="post-info">
+					<p>This post is written by farzana 2017/1/20</p>
+				</footer>
+				<content>
+				<p>
+<h4>FIRST, UNDERSTAND THAT STUDYING IS NOT THE SAME AS DOING HOMEWORK</h4>
+First, Understand that Studying is Not the Same as Doing Homework Learn 
+the difference between homework and studying, as well as how to approach each of these tasks.
+ Discover five tips for effective studying in college, and explore how factors such as 
+environment and outlook can affect your success while studying.</p> 
+<p>					<h4>REATE A STUDY PLANREATE A STUDY PLAN</h4>
+
+Create a Study PlanSee why it is critical to develop a study plan
+ in order to be successful in college. 
+Learn four steps to creating a realistic, effective plan of study,
+ and explore ways to ensure you follow through with your personalized plan.</p>
+ 
+
+
+				</content>
+
+			</article>
+				<article class="bottomarticle">
+				<header>
+					<h2><a href="#" title="second post">HOW TO STUDY BETTER</a></h2>
+
+				</header>
+				<footer class="post-info">
+					<p>This post is written by farzana jan 2017/1/20</p>
+				</footer>
+				<content>
+				<p>
+<p><h4>MANAGE YOUR TIME</h4>
+
+Manage Your TimeDiscover the recommended amount of study time per credit hour, 
+and see if you have been investing enough time in each course.
+ Find out how to avoid the mistake of studying too much,
+ and explore five tips for effective time management</p> 
+ <p><h4>AKE EFFECTIVE NOTES</h4>
+Take Effective NotesLearn how to save time and energy by using effective 
+note-taking strategies for both traditional and online classes. 
+Explore ways to exercise good judgment when deciding what to write down.
+You can maximize your productivity by leveraging these 
+tips for organizing and taking class notes.</p>
+
+				</content>
+
+			</article>
+
+		</div>
+
+	</div>
+	<aside class="top-sidebar">
+		<article>
+		<h2>what is colossus</h2>
+		<p>colossus was set of computer developedby british codwbreaker, 
+					in 1943-1945 to help in the cryptanalysisof lorintz cipher.</p>
+		</article>
+	</aside>
+	<aside class="middle-sidebar">
+		<article>
+		<h2>who designed</h2>
+		<p>colossus was designed by research telephone engineer Tommy Flowerto solve aproblem post by m athematician Max Newman .</p>
+		</article>
+	</aside>
+	<aside class="bottom-sidebar">
+		<article>
+		<h2>Programmer</h2>
+		<p>My view of colossuswas that of cryptanalyst programmaer.I told the macine to make certain calculation.</p>
+		</article>
+	</aside>
+	<footer class="mainFooter">
+		<p>copyright@<a href="#" title="myweb.com">myweb.com</a></p>
+	</footer>
+
+</body>
+</html>
